3b56b37a00004189db0bb7a7d1efe83e977c8f3d
wyatt-cordes-rvrvw <wyatt.cordes@rvrvw.org>
2025-04-14 18:56:39 +0000

    added check boxes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -42,9 +42,9 @@ if (localStorage.getItem('color-theme')) {
 
 async function verseLookup() {
     var verse = document.getElementById("search").value;
-    var headings = "true";
-    var extras = "true";
-    var numbers = "true";
+    var headings = document.getElementById("headings").checked;
+    var extras = document.getElementById("extras").checked;
+    var numbers = document.getElementById("numbers").checked;
     var url = "/api?verse=" + verse + "&headings=" + headings + "&extras=" + extras + "&numbers=" + numbers;
     fetch(url)
        .then(response => response.text())
@@ -59,3 +59,8 @@ inputField.addEventListener("keydown", function(event) {
     verseLookup();
   }
 });
+
+["headings", "extras", "numbers"].forEach(function(id) {
+    document.getElementById(id).addEventListener("change", verseLookup);
+});
+
